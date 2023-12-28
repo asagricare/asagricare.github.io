2678e100909dd58ce407e0ee8dc79039b5963ddd
asagricare <154879318+asagricare@users.noreply.github.com>
2023-12-28 22:09:24 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,33 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const imageContainers = document.querySelectorAll(".image-container");
+document.addEventListener('DOMContentLoaded', function () {
+  const imageContainers = document.querySelectorAll('.image-container');
+
+  let isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints;
 
   function toggleDetails(event) {
     const clickedContainer = event.currentTarget;
-    const details = clickedContainer.querySelector(".details");
+    const details = clickedContainer.querySelector('.details');
 
     // Close details for all other containers
     imageContainers.forEach(function (container) {
       if (container !== clickedContainer) {
-        container.querySelector(".details").classList.remove("active");
+        container.querySelector('.details').classList.remove('active');
       }
     });
 
     // Toggle details for the clicked container
-    details.classList.toggle("active");
+    details.classList.toggle('active');
   }
 
   function handleTouchEnd(event) {
-    event.preventDefault();
-    toggleDetails(event);
+    if (isTouchDevice) {
+      event.preventDefault();
+      toggleDetails(event);
+    }
   }
 
   // For both desktop and mobile
   imageContainers.forEach(function (container) {
-    container.addEventListener("click", toggleDetails);
-    container.addEventListener("touchend", handleTouchEnd);
+    container.addEventListener('click', toggleDetails);
+    container.addEventListener('touchend', handleTouchEnd);
   });
 });
